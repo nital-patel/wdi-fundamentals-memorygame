5ddf35107ace90e3e758bd2d1345b3669127dd14
Nital <patelnital36@gmail.com>
2017-07-19 23:39:31 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 var flipCard = function (cardId) {
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+
 
     if (cardsInPlay.length === 2) {
         if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -16,4 +40,3 @@ var flipCard = function (cardId) {
 
 flipCard(0);
 flipCard(2);
-
